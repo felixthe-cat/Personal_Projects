9a8556c1729cd9fc6cebfb3c57d9b75755123a97
felixthe-cat <66418097+felixthe-cat@users.noreply.github.com>
2022-06-17 00:26:59 +0100

    Completed analyse_row and merge_possible_values

--- SodukuSolver.py
+++ SodukuSolver.py
@@ -31,22 +31,38 @@ def ask_and_allocate(grid:list) -> list:
             output[row_index][index] = num
     return output
 
-def solve(soduku: list) -> list:
+def analyse_row(row: list) -> list:
+    # [3,0,0,4,0,8,5,1,0] check 3rd pos (index == 2)
+    number_list = [1,2,3,4,5,6,7,8,9]
+    for number in row:
+        if number in number_list:
+            number_list.remove(number)
+    return number_list
+
+def merge_possible_values(number_list: list, number_list1: list) -> list:
+    return list(set(number_list) - set(number_list1))
+    
+
+def solve(sudoku: list) -> list:
     # solves all the values in soudku and returns the result out
-    # calls a function on placeholder locations to analyse all the possible values the specific slot can be (coordinates: list, soduku: list) -> (possible_values: list)
+    
+    # calls a function on placeholder locations to analyse all the possible values the specific slot can be (coordinates: list, sudoku: list) -> (possible_values: list)
+    
+        # Possible_values = merge_possible_values(analyse_row(row), analyse_row(column))
         # function to analyse a row/column of elements to suggest possible values () 
-    # calls a function to scan all the possible_values list for a suitable value for one slot and returns the corrected soduku  (possible_values: list, soduku: list) -> ( coordinates: list, soduku: list)
+    # calls a function to scan all the possible_values list for a suitable value for one slot and returns the corrected sudoku  (possible_values: list, sudoku: list) -> ( coordinates: list, sudoku: list)
     # calls a function to update possible_values list (possible_values: list, coordinates: list) -> (possible_values: list)
-    # calls a function to check for any 0s in the soduku (soduku: list) -> (complete: bool)
+    # calls a function to check for any 0s in the sudoku (sudoku: list) -> (complete: bool)
     return 
 
 
-def check_validity(soduku: list) -> bool:
+def check_validity(sudoku: list) -> bool:
     # checkd whether there is a unique solution or not
+    # PAUSED
     return 
 
-soduku = []
-allotcate_placeholder_data(soduku)
-ask_and_allocate(soduku)
+sudoku = []
+allotcate_placeholder_data(sudoku)
+ask_and_allocate(sudoku)
 
-print_grid(soduku)
+print_grid(sudoku)
