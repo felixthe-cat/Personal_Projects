3f3012a94e2ba77361ba77a3395cd73103ea9b18
Felix <lawfelix2002@gmail.com>
2022-07-18 17:47:13 +0800

    Init

--- CEDD_fender_installation_predictor.py
+++ CEDD_fender_installation_predictor.py
@@ -1,1 +1,7 @@
-// https://data.weather.gov.hk/weatherAPI/opendata/opendata.php
+# https://data.weather.gov.hk/weatherAPI/opendata/opendata.php
+
+import requests
+
+r = requests.get('https://data.weather.gov.hk/weatherAPI/opendata/opendata.php?dataType=HHOT&station=CCH&year=2022&rformat=csv')
+
+print(r.content)
