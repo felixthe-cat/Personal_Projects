8454ce6a95913be576474cb3ff359fd86c7fda31
Law <lawfelix2002@gmail.com>
2022-09-03 18:21:07 +0800

    minor formatting CEG question

--- Part-time_RA/Part-Time_RA_CEG_Question_Trimmer.py
+++ Part-time_RA/Part-Time_RA_CEG_Question_Trimmer.py
@@ -11,7 +11,7 @@ def main():
         for question in question_list:
             if question != '':
                 if ':' not in question:
-                    if '(' not in question:
+                    # if '(' not in question:
                         new_question_list.append(question.strip())
         for index, question in enumerate(new_question_list):
             if question[-1] != '?':
@@ -21,9 +21,10 @@ def main():
                 new_question_list[index] = question[0].upper() + question[1:]
         print(new_question_list)
     with open ('Part-time_RA/Part-Time_RA_CEG_Questions_Output','w') as Answers:
-        for question in new_question_list:
-            Answers.write( " :::: " + question + ' {} \n\n')
-            # Answers.write()
+        for index, question in enumerate(new_question_list):
+            if '(' not in question:
+                Answers.write( " :: CEG_Question " + str(index) + " :: " + question + ' {} \n\n')
+                # Answers.write()
 
             
 
