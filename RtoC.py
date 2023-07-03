ba83c4e3bb1ae771b029018109dbdf448798d964
Law <lawfelix2002@gmail.com>
2023-07-03 11:36:18 +0800

    refined RtoC

--- RtoC.py
+++ RtoC.py
@@ -12,7 +12,7 @@ output = [i for i in output if i != '']
 
 # splits the para from the commenbt
 for index, item in enumerate(output):
-    output[index] = item.split(' – ')
+    output[index] = item.split(' - ')
 
 # removes header from para
 for index, item in enumerate(output):
@@ -35,22 +35,38 @@ with open('output.txt','w') as o:
 
 # output to excel
 my_wb = openpyxl.Workbook()
-my_sheet = openpyxl.load_workbook('output.xlsx').worksheets[0]
+my_sheet = my_wb.active
+
 
 # sets the font properties
 # my_sheet
 # my_sheet.style.font.name = 'Arial'
 # my_sheet.style.font.size = 8
 
-my_sheet.cell(row = 1, column = 1 ).value = 'ARRRR'
-my_sheet.cell(row = 1, column = 2 ).value = 'Section'
-my_sheet.cell(row = 1, column = 3 ).value = 'Comments'
+my_sheet.merge_cells('A1:C1') 
+my_sheet.cell(row = 1, column = 1 ).value = 'Departmental Comment'
 my_sheet.cell(row = 1, column = 4 ).value = 'Response to Comment'
+
+my_sheet.merge_cells('A2:C2') 
+my_sheet.cell(row = 2, column = 1 ).value = 'From: (By Email)\nContact: \nDate: 2023'
+my_sheet.cell(row = 2, column = 4 ).value = ""
+
+my_sheet.merge_cells('A3:C3') 
+my_sheet.cell(row = 3, column = 1 ).value = 'Departmental Comment'
+my_sheet.cell(row = 3, column = 4 ).value = ""
+
+adjustment = 4
+
+my_sheet.cell(row = adjustment, column = 1 ).value = 'Item'
+my_sheet.cell(row = adjustment, column = 2 ).value = 'Section'
+my_sheet.cell(row = adjustment, column = 3 ).value = 'Comments'
+my_sheet.cell(row = adjustment, column = 4 ).value = ""
+
 for index, _ in enumerate(output):
     if index != 0:
-        my_sheet.cell(row = index + 2, column = 1 ).value = index
-    my_sheet.cell(row = index + 2, column = 2 ).value = output[index][0]
-    my_sheet.cell(row = index + 2, column = 3 ).value = output[index][1]
-    my_sheet.cell(row = index + 2, column = 4 ).value = ''
+        my_sheet.cell(row = index + adjustment + 1, column = 1 ).value = index
+    my_sheet.cell(row = index + adjustment + 1, column = 2 ).value = output[index][0]
+    my_sheet.cell(row = index + adjustment + 1, column = 3 ).value = output[index][1]
+    my_sheet.cell(row = index + adjustment + 1, column = 4 ).value = ''
 my_wb.save('output.xlsx')
 
